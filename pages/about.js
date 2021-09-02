e2fe74aafa190411a6a4dfaf9e9084b87e2d88f4
jonhayjr <jonhayjr@gmail.com>
2021-09-01 17:50:09 -0700

    Intialized directory

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Labore irure ex amet do cillum duis. Amet occaecat aute ut amet. Cupidatat ea cupidatat aliqua voluptate elit culpa labore magna. Esse dolore aliquip nulla laboris excepteur exercitation commodo nostrud. Labore reprehenderit nisi esse officia reprehenderit excepteur consequat non qui minim non. Commodo fugiat do excepteur dolore ex ad duis. Anim Lorem deserunt voluptate eu consequat Lorem minim aliquip qui ad dolor.</p>
+                <p>Labore irure ex amet do cillum duis. Amet occaecat aute ut amet. Cupidatat ea cupidatat aliqua voluptate elit culpa labore magna. Esse dolore aliquip nulla laboris excepteur exercitation commodo nostrud. Labore reprehenderit nisi esse officia reprehenderit excepteur consequat non qui minim non. Commodo fugiat do excepteur dolore ex ad duis. Anim Lorem deserunt voluptate eu consequat Lorem minim aliquip qui ad dolor.</p>
+            </div>
+        </>
+    )
+}
+
+export default About
